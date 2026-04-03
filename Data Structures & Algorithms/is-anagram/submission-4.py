c02bfd2db413142b16fa035fc217e5432f545250
Abhishek Patel <abhishekpatel946@gmail.com>
2026-04-03 14:09:43 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,52 @@
+from collections import Counter
+
+class Solution:
+    def brute(self, s: str, t: str) -> bool:
+        # TC = o(nlogn + mlogm), SC = o(n + m)
+        s_ascii, t_ascii = [], []
+        for ch in s:
+            s_ascii.append(ord(ch))
+        for ch in t:
+            t_ascii.append(ord(ch))
+        return s_ascii.sort() == t_ascii.sort()
+    
+    def better(self, s: str, t: str) -> bool:
+        # edge case
+        if len(s) != len(t):
+            return False
+
+        # rearrange
+        i = k = 0
+        t_arr = list(t)
+        while k < len(s):
+            j = i + 1
+            if s[k] == t_arr[i]:
+                i += 1
+            else:
+                while j < len(t_arr):
+                    if s[k] == t_arr[j]:
+                        # swap
+                        t_arr[i], t_arr[j] = t_arr[j], t_arr[i]
+                        i += 1
+                        j += 1
+                        break
+                    j += 1
+            k += 1
+        # return
+        return s == ''.join(t_arr)
+
+    def optimal(self, s: str, t: str) -> bool:
+        # edge case
+        if len(s) != len(t):
+            return False
+        # map
+        s_map = Counter(s)
+        t_map = Counter(t)
+        for k, v in s_map.items():
+            if k not in t_map or t_map[k] != v:
+                return False
+        return True
+
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        return self.optimal(s, t)
